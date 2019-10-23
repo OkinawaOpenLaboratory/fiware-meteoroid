a986e01a5dc8504b2d252ab102898a1db880f8f0
YujiAzama <yuji.azama@gmail.com>
2019-10-23 18:07:06 +0900

    Add django-rest-swagger

--- meteoroid/meteoroid/settings.py
+++ meteoroid/meteoroid/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+  'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
--- meteoroid/meteoroid/urls.py
+++ meteoroid/meteoroid/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Meteoroid API')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('swagger', schema_view)
 ]
