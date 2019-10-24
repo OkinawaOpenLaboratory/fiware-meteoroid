df30754c02e5aec88ec66b911373c91047ea4c32
YujiAzama <yuji.azama@gmail.com>
2019-10-24 17:41:56 +0900

    Fix admin url

--- meteoroid/meteoroid/urls.py
+++ meteoroid/meteoroid/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.contrib import admin
 from django.urls import re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -33,6 +34,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+   re_path('admin/', admin.site.urls),
    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
