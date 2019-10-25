f9d87099e39fdfea15e8bae5bd836204059baa53
ryo-ma <saka_ro@yahoo.co.jp>
2019-10-25 07:39:20 +0200

    Add FIWAREBase model and Function model

--- meteoroid/core/models.py
+++ meteoroid/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class FIWAREBase(models.Model):
+    fiware_service = models.CharField(max_length=64, default='', blank=True)
+    fiware_service_path = models.CharField(max_length=64, default='/')
+
+    class Meta:
+        abstract = True
+
+
+class Function(FIWAREBase):
+    pass
