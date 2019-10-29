f07f9d02843eaad58633756addf99499bc368f90
ryo-ma <saka_ro@yahoo.co.jp>
2019-10-29 21:11:01 +0900

    Add FaaSDriver class

---
+++ meteoroid/core/libs/faas_driver.py
@@ -0,0 +1,51 @@
+from abc import ABCMeta, abstractmethod
+from .clients.open_whisk_client import OpenWhiskClient
+
+
+class FaaSDriver(metaclass=ABCMeta):
+    __instance = None
+    @classmethod
+    def get_faas_driver(cls):
+        # TODO: Obtain faas_name from environment
+        faas_name = 'open_whisk'
+        if not cls.__instance:
+            if faas_name == 'open_whisk':
+                cls.__instance = OpenWhiskDriver()
+        return cls.__instance
+
+    @abstractmethod
+    def list_function(self, fiware_service, fiware_service_path):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_function(self, function_id, fiware_service, fiware_service_path):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def create_function(self, fiware_service, fiware_service_path, data):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def update_function(self, function_id, fiware_service, fiware_service_path, data):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def delete_function(self, function_id, fiware_service, fiware_service_path):
+        raise NotImplementedError()
+
+
+class OpenWhiskDriver(FaaSDriver):
+    def list_function(self, fiware_service, fiware_service_path):
+        return OpenWhiskClient().list_function(fiware_service, fiware_service_path)
+
+    def get_function(self, function_id, fiware_service, fiware_service_path):
+        return OpenWhiskClient().get_function(function_id, fiware_service, fiware_service_path)
+
+    def create_function(self, fiware_service, fiware_service_path, data):
+        return OpenWhiskClient().get_function(fiware_service, fiware_service_path, data)
+
+    def update_function(self, function_id, fiware_service, fiware_service_path, data):
+        return OpenWhiskClient().get_function(function_id, fiware_service, fiware_service_path, data)
+
+    def delete_function(self, function_id, fiware_service, fiware_service_path):
+        return OpenWhiskClient().get_function(function_id, fiware_service, fiware_service_path)
