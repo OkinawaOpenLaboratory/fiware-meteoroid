bc173d89715910fec3753e8f762e76c38e6fdd3f
ryo-ma <saka_ro@yahoo.co.jp>
2019-10-29 21:10:39 +0900

    Add OpenWhiskClient

---
+++ meteoroid/core/libs/clients/open_whisk_client.py
@@ -0,0 +1,17 @@
+class OpenWhiskClient:
+
+    def list_function(self, fiware_service, fiware_service_path):
+        pass
+
+    def get_function(self, function_id, fiware_service, fiware_service_path):
+        pass
+
+    def create_function(self, fiware_service, fiware_service_path, data):
+        pass
+
+    def update_function(self, function_id, fiware_service, fiware_service_path, data):
+        pass
+
+    def delete_function(self, function_id, fiware_service, fiware_service_path):
+        pass
+
