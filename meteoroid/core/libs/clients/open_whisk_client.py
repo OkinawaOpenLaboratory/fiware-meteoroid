3e92b084948d6af7131d04068502b2d6cc475eea
ryo-ma <saka_ro@yahoo.co.jp>
2019-10-30 18:29:40 +0900

    Add OpenWhisk request info

--- meteoroid/core/libs/clients/open_whisk_client.py
+++ meteoroid/core/libs/clients/open_whisk_client.py
@@ -1,7 +1,20 @@
+import requests
+import os
+
+
 class OpenWhiskClient:
+    def __init__(self):
+        self.api_key = os.environ.get('OPEN_WHISK_API_KEY', '')
+        self.host = os.environ.get('OPEN_WHISK_HOST', '')
+        self.endpoint = f'https://{self.host}'
+        self.headers = {
+            'Authorization': f'Basic {self.api_key}',
+            'User-Agent': 'fiware-meteoroid/0.1'
+        }
 
     def get_function_list(self, fiware_service, fiware_service_path):
-        pass
+        response = requests.get('{self.endpoint}/api/v1/namespaces/_/actions', headers=self.headers, verify=False)
+        return response.json()
 
     def get_function(self, function_id, fiware_service, fiware_service_path):
         pass
