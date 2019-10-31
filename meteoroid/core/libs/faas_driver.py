c4c6e1c5e3337ef15311eeb27bc82f676c2f6789
ryo-ma <saka_ro@yahoo.co.jp>
2019-10-31 11:06:10 +0900

    Add escape method to OpenWhiskDriver

--- meteoroid/core/libs/faas_driver.py
+++ meteoroid/core/libs/faas_driver.py
@@ -35,17 +35,32 @@ class FaaSDriver(metaclass=ABCMeta):
 
 
 class OpenWhiskDriver(FaaSDriver):
+    def escape_fiware_service_path(self, fiware_service_path):
+        ESCAPE_TARGET_STR = '/'
+        ESCAPE_NEW_STR = '-'
+        return fiware_service_path.replace(ESCAPE_TARGET_STR, ESCAPE_NEW_STR)
+
     def get_function_list(self, fiware_service, fiware_service_path):
-        return OpenWhiskClient().get_function_list(fiware_service, fiware_service_path)
+        escaped_fiware_service_path = self.escape_fiware_service_path(fiware_service_path)
+        namespace = f'{fiware_service}{escaped_fiware_service_path}'
+        return OpenWhiskClient().get_function_list(namespace)
 
     def get_function(self, function_id, fiware_service, fiware_service_path):
-        return OpenWhiskClient().get_function(function_id, fiware_service, fiware_service_path)
+        escaped_fiware_service_path = self.escape_fiware_service_path(fiware_service_path)
+        namespace = f'{fiware_service}{escaped_fiware_service_path}'
+        return OpenWhiskClient().get_function(function_id, namespace)
 
-    def create_function(self, fiware_service, fiware_service_path, data):
-        return OpenWhiskClient().create_or_update_function(fiware_service, fiware_service_path, data)
+    def create_function(self, namespace, data, fiware_service, fiware_service_path):
+        escaped_fiware_service_path = self.escape_fiware_service_path(fiware_service_path)
+        namespace = f'{fiware_service}{escaped_fiware_service_path}'
+        return OpenWhiskClient().create_or_update_function(namespace, data)
 
     def update_function(self, function_id, fiware_service, fiware_service_path, data):
-        return OpenWhiskClient().create_or_update_function(function_id, fiware_service, fiware_service_path, data)
+        escaped_fiware_service_path = self.escape_fiware_service_path(fiware_service_path)
+        namespace = f'{fiware_service}{escaped_fiware_service_path}'
+        return OpenWhiskClient().create_or_update_function(function_id, namespace, data)
 
     def delete_function(self, function_id, fiware_service, fiware_service_path):
-        return OpenWhiskClient().delete_function(function_id, fiware_service, fiware_service_path)
+        escaped_fiware_service_path = self.escape_fiware_service_path(fiware_service_path)
+        namespace = f'{fiware_service}{escaped_fiware_service_path}'
+        return OpenWhiskClient().delete_function(function_id, namespace)
