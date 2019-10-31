25833fdc85a529381e5559615d4b5ead071dc9a9
ryo-ma <saka_ro@yahoo.co.jp>
2019-10-31 13:43:45 +0900

    Fix OpenWhiskClient auth

--- meteoroid/core/libs/clients/open_whisk_client.py
+++ meteoroid/core/libs/clients/open_whisk_client.py
@@ -4,16 +4,19 @@ import os
 
 class OpenWhiskClient:
     def __init__(self):
-        self.api_key = os.environ.get('OPEN_WHISK_API_KEY', '')
+        self.user = os.environ.get('OPEN_WHISK_USER', '')
+        self.password = os.environ.get('OPEN_WHISK_PASSWORD', '')
         self.host = os.environ.get('OPEN_WHISK_HOST', '')
         self.endpoint = f'https://{self.host}'
         self.headers = {
-            'Authorization': f'Basic {self.api_key}',
             'User-Agent': 'fiware-meteoroid/0.1'
         }
 
     def get_function_list(self, namespace):
-        response = requests.get('{self.endpoint}/api/v1/namespaces/_/actions', headers=self.headers, verify=False)
+        response = requests.get(f'{self.endpoint}/api/v1/namespaces/{namespace}/actions',
+                                headers=self.headers,
+                                auth=(self.user, self.password),
+                                verify=False)
         return response.json()
 
     def get_function(self, function_id, namespace):
