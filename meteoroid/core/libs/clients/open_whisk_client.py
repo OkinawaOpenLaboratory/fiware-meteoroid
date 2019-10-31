5d3dc2f7f791f03e125ab19234e6f30a0b943800
ryo-ma <saka_ro@yahoo.co.jp>
2019-10-31 09:17:27 +0900

    Change fiware_service and fiware_service_path of OpenWhiskCliewnt to namespace

--- meteoroid/core/libs/clients/open_whisk_client.py
+++ meteoroid/core/libs/clients/open_whisk_client.py
@@ -12,16 +12,16 @@ class OpenWhiskClient:
             'User-Agent': 'fiware-meteoroid/0.1'
         }
 
-    def get_function_list(self, fiware_service, fiware_service_path):
+    def get_function_list(self, namespace):
         response = requests.get('{self.endpoint}/api/v1/namespaces/_/actions', headers=self.headers, verify=False)
         return response.json()
 
-    def get_function(self, function_id, fiware_service, fiware_service_path):
+    def get_function(self, function_id, namespace):
         pass
 
-    def create_or_update_function(self, fiware_service, fiware_service_path, data):
+    def create_or_update_function(self, namespace, data):
         pass
 
-    def delete_function(self, function_id, fiware_service, fiware_service_path):
+    def delete_function(self, function_id, namespace):
         pass
 
