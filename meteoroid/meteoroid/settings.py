493d249e6d1d5a569451545b15fc81fbd94a0ee3
ryo-ma <saka_ro@yahoo.co.jp>
2019-11-08 17:32:37 +0900

    Support to core api

--- meteoroid/meteoroid/settings.py
+++ meteoroid/meteoroid/settings.py
@@ -107,6 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+REST_FRAMEWORK = {
+  'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
 
 
 # Internationalization
