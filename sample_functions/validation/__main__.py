9b8994d1804ed3da36afe7d092ddf4ea16342e0f
ryo-ma <saka_ro@yahoo.co.jp>
2020-09-04 20:16:22 +0900

    Support to convert ngsi entity

--- sample_functions/validation/__main__.py
+++ sample_functions/validation/__main__.py
@@ -1,5 +1,5 @@
 import json
-import requests
+import uuid
 from jsonschema import validate, ValidationError
 
 schema = {}
@@ -9,9 +9,39 @@ with open('./schema.json') as f:
     schema = json.load(f)
 
 
+def convert_to_ngsi_entity(data, schema):
+    entity_id = str(uuid.uuid4())
+    entity_type = schema['type']
+    result = {}
+    if 'id' in data:
+        entity_id = data['id']
+    if 'type' in data:
+        entity_type = data['type']
+    result['id'] = entity_id
+    result['type'] = entity_type
+    attrs = {k: v for k, v in data.items() if k not in ['id', 'type']}
+    for k, v in attrs.items():
+        attr_type = schema['properties'][k]['type']
+        result[k] = {
+            'value': data[k],
+            'type': attr_type
+        }
+    return result
+
+
 def main(args):
     try:
         validate(instance=args, schema=schema)
-        return {'validation': 'success'}
+        entity = convert_to_ngsi_entity(args, schema)
+        return {'validation': 'success', 'entity': entity}
     except ValidationError as e:
         return {'validation': 'failed', 'reason': e.message}
+
+
+if __name__ == '__main__':
+    args = {
+        "id": "Grocery1",
+        "name": "Eggs",
+        "price": 34.99
+    }
+    print(main(args))
